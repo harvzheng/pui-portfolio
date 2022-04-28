844aad9c9442676626df13eb369fa7ef06f148bd
Harvey Zheng <zheng.harvey5@gmail.com>
2022-04-27 20:35:23 -0400

    minor nav change

--- components/nav.js
+++ components/nav.js
@@ -13,9 +13,9 @@ export default function Nav() {
   return (
     <>
       <Container px={4} maxW='container.xl' className="py-3">
-        <Flex h={16} alignItems={'center'} justifyContent={'space-between'} className="px-5 md:px-14 ">
-          <HStack spacing={8} alignItems={'center'}>
-            <Box>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+          <HStack spacing={8} alignItems={'center'} >
+            <Box className="px-4 py-3" style={{border: "1px solid white"}}>
               <Link href="/" color="gray.50">
                 Harvey Zheng
               </Link>
